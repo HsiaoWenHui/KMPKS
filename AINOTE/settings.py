cd3f98f85a3937215986184325b6c94cfe98b522
Joyce Hsiao <1205hsiao@gmail.com>
2020-09-30 18:33:49 +0800

    set redis url

--- AINOTE/settings.py
+++ AINOTE/settings.py
@@ -175,7 +175,7 @@ CHANNEL_LAYERS = {
       'default': {
           'BACKEND': 'channels_redis.core.RedisChannelLayer',
           'CONFIG': {
-               "hosts": [os.environ.get('HEROKU_REDIS_YELLOW_URL', 'redis://localhost:6379')],
+               "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
           },
       },
   }
