53402178c69b187e4a0f0ed2d5026acd1a3f1361
Joyce Hsiao <1205hsiao@gmail.com>
2020-10-10 17:18:21 +0800

    群組搜尋功能修正

--- home/context_processors.py
+++ home/context_processors.py
@@ -3,12 +3,16 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from personal.models import UserProfile
 from article.models import article,tag
-from group.models import member
+from group.models import member,group
 from django.db.models.aggregates import Count
 def getHotKeyword(request):
     tag_list = tag.objects.annotate(num_posts=Count('article')).order_by("-num_posts")[:10]
-    group_list=member.objects.annotate(num_member=Count('groupID')).order_by("-num_member")[:10]
-
+    member_list=member.objects.values("groupID__id").annotate(Count('memberID'))
+    group_list=[]
+    for i in member_list:
+        g=group.objects.get(id=i.get('groupID__id'))
+        if g.privacy == 1:
+            group_list.append(g)
     return {
         'hot_tag': tag_list,
         'hot_group':group_list
